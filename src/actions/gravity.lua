9ebc3f6e8e5a1450f231f5740ffa081805bef50c
orenjiakira <orenjiakira@gmail.com>
2016-11-23 02:34:22 -0200

    Add jump

--- src/actions/gravity.lua
+++ src/actions/gravity.lua
@@ -5,10 +5,10 @@ local Action = require 'action'
 
 local self = Action.inherit()
 
-local G = require 'basic.vector' :new { 0, 10 }
+local G = require 'basic.vector' :new { 0, 6 }
 
 self:add("update_physics", function (id)
-  if not GameState.__initialized then return end
+    if not GameState.__initialized then return end
 
   local player_body = Manager:get_component(id, 'body')
 
