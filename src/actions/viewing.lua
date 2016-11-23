9782bb7c0f67fe3b7518ed6fd136bd36a3212500
orenjiakira <orenjiakira@gmail.com>
2016-11-23 20:21:03 -0200

    Make the players look at each other

--- src/actions/viewing.lua
+++ src/actions/viewing.lua
@@ -12,6 +12,27 @@ self:add("physics_done", function (id)
   local player_sprite = Manager:get_component(id, 'sprite')
 
   player_sprite:set_position(player_body.physics:get_pos())
+end)
+
+self:add("update_physics", function (id)
+  local otherplayer = GameState:get_player1()
+  if id == otherplayer then
+    otherplayer = GameState:get_player2()
+  end
+
+  local player_body = Manager:get_component(id, 'body')
+  local otherplayer_body = Manager:get_component(otherplayer, 'body')
+
+  local player_sprite = Manager:get_component(id, 'sprite')
+  local otherplayer_sprite = Manager:get_component(id, 'sprite')
+
+  if player_body.physics:get_pos().x < otherplayer_body.physics:get_pos().x then
+    player_sprite:set_flip_h(false)
+  end
+
+  if player_body.physics:get_pos().x > otherplayer_body.physics:get_pos().x then
+    player_sprite:set_flip_h(true)
+  end
 
 end)
 
